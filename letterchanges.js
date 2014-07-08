577f531eb399ce689512cee8bc5b3444d871b86b
Kyle Maune <kmaune71@gmail.com>
2014-07-08 14:23:42 -0700

    initial commit

---
+++ letterchanges.js
@@ -0,0 +1,25 @@
+// letterchanges.js
+// (C) 2014 splashinn
+
+function LetterChanges(str) { 
+  var vowels = "aeiou";
+  var alpha = "abcdefghijklmnopqrstuvwxyz";
+  var shifted = str.split("");
+  var str = "";
+  // code goes here  
+  for(var i = 0; i < shifted.length; i++) {
+    if (shifted[i] == "z"){
+      str += "A";
+    } else if (alpha.indexOf(shifted[i].toLowerCase()) != -1) {
+      if (vowels.indexOf(alpha[alpha.indexOf(shifted[i].toLowerCase()) + 1]) != -1){
+        str += alpha[alpha.indexOf(shifted[i].toLowerCase()) + 1].toUpperCase();
+      } else {
+        str += alpha[alpha.indexOf(shifted[i].toLowerCase()) + 1];
+      } 
+    } else {
+      str += shifted[i];
+    }
+  }
+  return str; 
+         
+}
