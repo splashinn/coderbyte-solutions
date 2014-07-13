f80ef4c7065a050771203f51ba2e3bf3ae112a04
Kyle Maune <kmaune71@gmail.com>
2014-07-13 10:11:29 -0700

    changed comments

--- wordcount.js
+++ wordcount.js
@@ -1,6 +1,6 @@
-// wordcount.js
+// wordcount.js solution for Coderbyte
 // (C) 2014 splashinn
 
-function WordCount(str) { 
-  return str.split(" ").length; 
+function WordCount(str) {
+  return str.split(" ").length;
 };
