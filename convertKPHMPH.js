b2474fcd1d70e3e9fcc4243fd0575bb752dead63
Kyle Maune <kmaune71@gmail.com>
2014-07-22 20:46:37 -0700

    added conversion for kph to mph

---
+++ convertKPHMPH.js
@@ -0,0 +1,14 @@
+// kph to mph
+// splashinn
+
+function convert(speed) {
+  return speed/1.609344;
+}
+console.log(convert());
+
+// mph to kph
+
+function convert2(speed) {
+  return speed * 1.609344;
+}
+console.log(convert());
